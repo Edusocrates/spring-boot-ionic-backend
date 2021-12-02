898de1d375eccfe54c7c4837b54de52a3b853a3a
edusocrates <edu5390@gmail.com>
2021-12-02 16:11:19 -0300

    criando objeto categoria

--- src/main/java/com/edusocrates/cursoMC/controller/CategoriaController.java
+++ src/main/java/com/edusocrates/cursoMC/controller/CategoriaController.java
@@ -1,15 +1,25 @@
 package com.edusocrates.cursoMC.controller;
 
+import com.edusocrates.cursoMC.model.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar(){
-        return "Rest esta funcionando!";
+    public List<Categoria> listar(){
+        Categoria categoria = new Categoria(1,"informatica");
+        Categoria categoria2 = new Categoria(2,"Escritorio");
+
+        List<Categoria> list = new ArrayList<>();
+        list.add(categoria);
+        list.add(categoria2);
+        return list;
     }
 }
--- src/main/java/com/edusocrates/cursoMC/model/Categoria.java
+++ src/main/java/com/edusocrates/cursoMC/model/Categoria.java
@@ -1,4 +1,51 @@
 package com.edusocrates.cursoMC.model;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.Objects;
+//
+public class Categoria implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+
+
+    private Integer id;
+
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Categoria() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId()) && getNome().equals(categoria.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome());
+    }
 }
